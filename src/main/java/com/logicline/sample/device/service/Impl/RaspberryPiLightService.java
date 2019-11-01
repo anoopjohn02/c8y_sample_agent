ad406986ed2a93f79e9b3e0be5a658b4060c3a5e
Anoop John <anoop.john@logicline.de>
2019-11-01 11:24:18 +0530

    added logs

--- src/main/java/com/logicline/sample/device/service/Impl/RaspberryPiLightService.java
+++ src/main/java/com/logicline/sample/device/service/Impl/RaspberryPiLightService.java
@@ -1,31 +1,40 @@
 package com.logicline.sample.device.service.Impl;
 
+import com.logicline.sample.agent.drivers.LightDriver;
 import com.logicline.sample.device.service.LightService;
 import com.pi4j.io.gpio.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class RaspberryPiLightService implements LightService {
+
+    private static final Logger logger = LoggerFactory.getLogger(RaspberryPiLightService.class);
 
     private GpioPinDigitalOutput pin;
 
     @Override
     public void init(){
+        logger.info("RaspberryPiLight Operation - init ");
         GpioController gpio = GpioFactory.getInstance();
         pin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_03, "MyLED", PinState.HIGH);
-        pin.low();
+        //pin.low();
     }
 
     @Override
     public void switchOn() {
+        logger.info("RaspberryPiLight Operation - On ");
         pin.high();
     }
 
     @Override
     public void switchOff() {
+        logger.info("RaspberryPiLight Operation - Off ");
         pin.low();
     }
 
     @Override
     public void toggle() {
+        logger.info("RaspberryPiLight Operation - toggle ");
         pin.toggle();
     }
 }
