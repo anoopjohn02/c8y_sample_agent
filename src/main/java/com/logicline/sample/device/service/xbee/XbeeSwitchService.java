8c9b817b00b3b585fd4bcdb2d96dbeff1e4644f4
Anoop John <anoop.john@logicline.de>
2019-11-06 18:30:42 +0530

    Xbee Switch sample update

--- src/main/java/com/logicline/sample/device/service/xbee/XbeeSwitchService.java
+++ src/main/java/com/logicline/sample/device/service/xbee/XbeeSwitchService.java
@@ -1,22 +1,58 @@
 package com.logicline.sample.device.service.xbee;
 
+import com.digi.xbee.api.WiFiDevice;
+import com.digi.xbee.api.XBeeDevice;
+import com.digi.xbee.api.models.XBeeProtocol;
 import com.logicline.sample.device.service.SwitchService;
+import com.logicline.sample.device.service.raspberry.RaspberryPiLightService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class XbeeSwitchService implements SwitchService {
 
+    private static final Logger logger = LoggerFactory.getLogger(XbeeSwitchService.class);
+
+    private static final String PORT = "/dev/ttyAMA0/";
+    private static final int BAUD_RATE = 9600;
+    private XBeeDevice xbeeDevice;
 
     @Override
     public void init() {
-
+        xbeeDevice = new XBeeDevice(PORT, BAUD_RATE);
     }
 
     @Override
     public void switchOn(String switchId) {
-
+        logger.info("XBee Operation - On");
+        String dataToSend = switchId+"_ON";
+        sendData(dataToSend);
     }
 
     @Override
     public void switchOff(String switchId) {
+        logger.info("XBee Operation - Off ");
+        String dataToSend = switchId+"_OFF";
+        sendData(dataToSend);
+    }
 
+    private void sendData(String data){
+        try {
+            byte[] dataToSend = data.getBytes();
+            xbeeDevice.open();
+
+            if (xbeeDevice.getXBeeProtocol() == XBeeProtocol.XBEE_WIFI) {
+                xbeeDevice.close();
+                xbeeDevice = new WiFiDevice(PORT, BAUD_RATE);
+                xbeeDevice.open();
+                ((WiFiDevice)xbeeDevice).sendBroadcastIPData(0x2616, dataToSend);
+            } else{
+                xbeeDevice.sendBroadcastData(dataToSend);
+            }
+        } catch (Exception e) {
+            logger.error("Error occured during sending to XBee device ", e);
+        }
+        finally {
+            xbeeDevice.close();
+        }
     }
 }
